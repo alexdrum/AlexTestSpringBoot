3269070db8825f5ae0a3c7e4bbd25374ff0a9042
zhaojw30 <zhaojw30@ziroom.com>
2017-07-27 15:42:42 +0800

    Initial commit

---
+++ src/main/java/com/alextest/AlexTestSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.alextest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexTestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexTestSpringBootApplication.class, args);
+	}
+}
